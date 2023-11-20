f720f43e37715b9b2f5f726ea54ee501b7696d8e
Marios Zoutis <coding.pharynxes@aleeas.com>
2023-11-20 19:07:31 +0200

    Add ability for current user to update their info

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,5 +1,16 @@
 const User = require('../models/userModel');
 const catchAsync = require('../utils/catchAsync');
+const AppError = require('../utils/appError');
+
+const filterObj = (obj, ...allowedFields) => {
+  const newObj = {};
+  Object.keys(obj).forEach((key) => {
+    if (allowedFields.includes(key)) {
+      newObj[key] = obj[key];
+    }
+  });
+  return newObj;
+};
 
 exports.getAllUsers = catchAsync(async (req, res, next) => {
   const users = await User.find();
@@ -12,6 +23,29 @@ exports.getAllUsers = catchAsync(async (req, res, next) => {
       users,
     },
   });
+});
+
+exports.updateMe = catchAsync(async (req, res, next) => {
+  // 1) Create error if user's request contains a password change
+  if (req.body.password || req.body.passwordConfirm) {
+    return next(
+      new AppError(
+        'This route is not for password updates. Please use /updateMyPassword.',
+        400
+      )
+    );
+  }
+
+  // 2) Update user document
+  // Create a filter that contains only the fields that a user is allowed to update for security reasons.
+  // For example a user should not be able to change their 'role' to 'admin'
+  const filteredBody = filterObj(req.body, 'name', 'email');
+  const updatedUser = await User.findByIdAndUpdate(req.user.id, filteredBody, {
+    runValidators: true,
+    new: true, // returns the updated user in response
+  });
+
+  res.status(200).json({ status: 'success', data: { user: updatedUser } });
 });
 
 exports.getUser = (req, res) => {
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -13,8 +13,10 @@ router.patch('/resetPassword/:token', authController.resetPassword);
 router.patch(
   '/updateMyPassword',
   authController.protect,
-  authController.updatePassword
+  authController.updateMyPassword
 );
+
+router.patch('/updateMe', authController.protect, userController.updateMe);
 
 router
   .route('/')
