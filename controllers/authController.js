986dcd38417833bbca886173c325f3656c2d3dfd
Marios Zoutis <coding.pharynxes@aleeas.com>
2023-11-24 19:47:58 +0200

    Send JWT with a cookie

--- controllers/authController.js
+++ controllers/authController.js
@@ -14,6 +14,18 @@ const signToken = (id) => {
 
 const createSendToken = (user, statusCode, res) => {
   const token = signToken(user._id);
+  const cookieOptions = {
+    expires: new Date(
+      Date.now() + process.env.JWT__COOKIE_EXPIRES_IN * 24 * 60 * 60 * 1000
+    ),
+    httpOnly: true,
+  };
+
+  if (process.env.NODE_ENV === 'production') cookieOptions.secure = true;
+  res.cookie('jwt', token, cookieOptions);
+
+  // Remove the password from the output, not the database
+  user.password = undefined;
 
   res.status(statusCode).json({
     status: 'success',
