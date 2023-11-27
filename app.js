ec5e509f82d497f9d06a8de0045bfd1298e70296
Marios Zoutis <coding.pharynxes@aleeas.com>
2023-11-27 19:57:01 +0200

    Add mechanisms to prevent common security exploitation

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const morgan = require('morgan');
 const rateLimit = require('express-rate-limit');
+const helmet = require('helmet');
+const mongoSanitize = require('express-mongo-sanitize');
+const { xss } = require('express-xss-sanitizer');
+const hpp = require('hpp');
 
 const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
@@ -10,12 +14,17 @@ const globalErrorController = require('./controllers/errorController');
 const app = express();
 
 // 1) MIDDLEWARES
-// run the logger package only if we are in development environment
+
+// Set security HTTP headers
+app.use(helmet());
+
+// Run the logger package only if we are in development environment
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
 }
 
 // Add this limiter middleware to mitigate DDOS and Brute Force attacks
+// Limits requests from same IP
 const limiter = rateLimit({
   max: 100,
   windowMs: 60 * 60 * 1000,
@@ -23,9 +32,34 @@ const limiter = rateLimit({
 });
 
 app.use('/api', limiter);
-app.use(express.json());
+
+// Body parser. Reads data from body into req.body
+app.use(express.json({ limit: '10kb' }));
+
+// Data sanitization against NoSQL query injection
+app.use(mongoSanitize());
+
+// Data sanitization against XSS
+app.use(xss());
+
+// Prevent HTTP parameter polution
+app.use(
+  hpp({
+    whitelist: [
+      'duration',
+      'ratingsQuantity',
+      'ratingsAverage',
+      'maxGroupSize',
+      'difficulty',
+      'price',
+    ],
+  })
+);
+
+// Serve static files
 app.use(express.static(`${__dirname}/public`));
 
+// Test middleware
 app.use((req, res, next) => {
   req.requestTime = new Date().toISOString();
   next();
