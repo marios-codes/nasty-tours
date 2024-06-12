084c65a3b3303fb9dec75e13f7ee92a475470ce1
marioszou <marioszou@proton.me>
2024-06-12 17:58:52 +0300

    Refactor pug views code to respect MVC architecture

--- app.js
+++ app.js
@@ -10,6 +10,7 @@ const hpp = require('hpp');
 const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
 const reviewRouter = require('./routes/reviewRoutes');
+const viewRouter = require('./routes/viewRoutes');
 const AppError = require('./utils/appError');
 const globalErrorController = require('./controllers/errorController');
 
@@ -72,25 +73,7 @@ app.use((req, res, next) => {
 });
 
 // 3) ROUTES (MOUNTING ROUTES)
-app.get('/', (req, res) => {
-  res.status(200).render('base', {
-    tour: 'The Forest Hiker',
-    user: 'Marios',
-  });
-});
-
-app.get('/overview', (req, res) => {
-  res.status(200).render('overview', {
-    title: 'All Tours',
-  });
-});
-
-app.get('/tour', (req, res) => {
-  res.status(200).render('tour', {
-    title: 'The Forest Hiker Tour',
-  });
-});
-
+app.use('/', viewRouter);
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
 app.use('/api/v1/reviews', reviewRouter);
