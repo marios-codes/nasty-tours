c00017f3f6e268b19e2a1b302d8ca693a4023376
Marios Zou <coding.pharynxes@aleeas.com>
2024-07-31 16:00:29 +0300

    Fix bug that did not create new booking upon payment

--- controllers/bookingController.js
+++ controllers/bookingController.js
@@ -76,7 +76,7 @@ exports.webhookCheckout = (req, res, next) => {
     return res.status(400).send(`Webhook error: ${error.message}`);
   }
 
-  if (event === 'checkout.session.completed')
+  if (event.type === 'checkout.session.completed')
     createBookingCheckout(event.data.object);
 
   res.status(200).json({ received: true });
