5bc8c31ec39da2f236a58ab7d119f6b2c9178185
Marios Zou <coding.pharynxes@aleeas.com>
2024-08-01 16:19:50 +0300

    Add additonal check for reviewers

--- controllers/reviewController.js
+++ controllers/reviewController.js
@@ -1,4 +1,7 @@
+const Booking = require('../models/bookingModel');
 const Review = require('../models/reviewModel');
+const AppError = require('../utils/appError');
+const catchAsync = require('../utils/catchAsync');
 // const catchAsync = require('../utils/catchAsync');
 const factory = require('./handlerFactory');
 
@@ -8,6 +11,17 @@ exports.setTourUserIds = (req, res, next) => {
   if (!req.body.user) req.body.user = req.user.id;
   next();
 };
+
+// Check if the user has actually booked that tour
+// in order to be able to leave a review
+exports.checkUserBooking = catchAsync(async (req, res, next) => {
+  const { tour, user } = req.body;
+
+  const booking = await Booking.findOne({ tour: tour, user: user });
+  if (!booking)
+    return next(new AppError('Cannot find booking for selected user.', 404));
+  next();
+});
 
 exports.getAllReviews = factory.getAll(Review);
 exports.getReview = factory.getOne(Review);
